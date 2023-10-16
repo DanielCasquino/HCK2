323b1393a58f71b5dd412d188140b6acab0159cc
Snah-s <camiloernestosoto@gmail.com>
2023-10-16 16:47:35 -0500

    ✨  GroupController added
    Group Controller is added in src directory

---
+++ demo/src/main/java/com/example/demo/GroupController.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import java.util.*;
+
+import javax.swing.GroupLayout.Group;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/group")
+public class GroupController {
+    
+    @Autowired
+    private GroupController GroupRepository;
+
+    @Autowired
+    private PersonRepository PersonRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Group>> group() {
+        List<Group> groups = GroupRepository.findAll();
+        return new ResponseEntity<>(groups, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Group> getGroups(@PathVariable Long id) {
+        Optional<Group> query = GroupRepository.findById(id);
+        return new ResponseEntity<>(query.get(), HttpStatus.OK);
+    }
+
+    @GetMapping("/person/{id}")
+    public ResponseEntity<List<Group>> getPersonGroups(@PathVariable Long id) {
+        Optional<Person> query = PersonRepository.findById(id);
+        if (query.isPresent()) {
+            return new ResponseEntity<>(query.get().getGroups(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
